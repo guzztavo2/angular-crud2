3b8015b5b721ede139fd9c84a13080bdf16ba495
guzztavo2 <gustavo_bonifacio2020@outlook.com>
2024-01-17 16:37:03 -0300

    update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+
 import { RouterLink, Router } from '@angular/router';
 import { RegistroComponent } from '../registro/registro.component';
 import { AuthService } from '../auth/auth.service';
+import { CommonModule } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import User from '../auth/user';
+import { Modal } from '../modal/modal';
+import { ModalComponent } from '../modal/modal.component';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, RegistroComponent],
+  imports: [
+    RouterLink,
+    RegistroComponent,
+    CommonModule,
+    ReactiveFormsModule,
+    ModalComponent,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
@@ -15,6 +32,34 @@ export class LoginComponent {
       if (isLoggedIn) {
         router.navigateByUrl('/');
       }
+    });
+  }
+
+  public loadingModal: Modal = {
+    title: 'Sua requisição está sendo carregada! ⏳',
+    description: '',
+    canClose: false,
+    showFooter: false,
+    visibility: false,
+    display: false,
+    loadingIcon: true,
+    redirect: false,
+  };
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  });
+
+  submitForm() {
+    this.loadingModal.display = true;
+    this.loadingModal.visibility = true;
+
+    new Promise((ex) => {
+      const user = User.getUserFromUsersList(
+        this.loginForm.get('email')?.value as string,
+        this.loginForm.get('password')?.value as string
+      );
     });
   }
 }
--- src/app/logout/logout.component.ts
+++ src/app/logout/logout.component.ts
@@ -3,11 +3,9 @@ import { AuthService } from '../auth/auth.service';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-logout',
   standalone: true,
   imports: [],
-  templateUrl: './logout.component.html',
-  styleUrl: './logout.component.css',
+  template: '',
 })
 export class LogoutComponent {
   constructor(authService: AuthService, router: Router) {
